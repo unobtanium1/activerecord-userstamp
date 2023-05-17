2c4b154d9bcb8697de9aa9ddbf11445797f4984d
unobtanium1 <63619795+unobtanium1@users.noreply.github.com>
2023-05-17 23:55:37 +0000

    Update stampable.rb (arity fix due to change in belongs_to: signature to process scope during builder eval

--- lib/active_record/userstamp/stampable.rb
+++ lib/active_record/userstamp/stampable.rb
@@ -80,15 +80,30 @@ module ActiveRecord::Userstamp::Stampable
       klass = stamper_class.try(:name)
       relation_options = options.reverse_merge(optional: true, class_name: klass)
 
-      belongs_to :creator, relation_options.reverse_merge(foreign_key: config.creator_attribute) if
+      # belongs_to :creator, relation_options.reverse_merge(foreign_key: config.creator_attribute) if
+      #   associations.first
+      # belongs_to :updater, relation_options.reverse_merge(foreign_key: config.updater_attribute) if
+      #   associations.second
+      # if associations.third
+      #   relation_options.reverse_merge!(required: false) if ActiveRecord::VERSION::MAJOR >= 5 ||
+      #     (ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR >= 2)
+      #   belongs_to :deleter, relation_options.reverse_merge(foreign_key: config.deleter_attribute)
+      # end
+      
+      #fixed arity error (hash has no arity).. problem was belongs_to now takes scope as well so options 
+      #were being passed in as hash and was being passed into proc check and arity was incorrectly called
+      #solution:  ** derefence hash
+      belongs_to :creator, **relation_options.reverse_merge(foreign_key: config.creator_attribute) if
         associations.first
-      belongs_to :updater, relation_options.reverse_merge(foreign_key: config.updater_attribute) if
+      belongs_to :updater, **relation_options.reverse_merge(foreign_key: config.updater_attribute) if
         associations.second
       if associations.third
         relation_options.reverse_merge!(required: false) if ActiveRecord::VERSION::MAJOR >= 5 ||
           (ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR >= 2)
-        belongs_to :deleter, relation_options.reverse_merge(foreign_key: config.deleter_attribute)
+        belongs_to :deleter, **relation_options.reverse_merge(foreign_key: config.deleter_attribute)
       end
+      
+      
     end
   end
 
